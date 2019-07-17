37c99b48c1dfa9af16d18e9f308ceab5cb566ab3
strval <strval@qq.com>
2019-07-17 15:06:08 +0800

    初始化

---
+++ tpcms.sql
@@ -0,0 +1,174 @@
+﻿# Host: localhost  (Version: 5.5.53)
+# Date: 2019-07-11 11:23:36
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "tp_admin"
+#
+
+DROP TABLE IF EXISTS `tp_admin`;
+CREATE TABLE `tp_admin` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL DEFAULT '' COMMENT '用户名',
+  `password` varchar(40) NOT NULL DEFAULT '' COMMENT '密码',
+  `avatar` varchar(255) DEFAULT NULL COMMENT '头像',
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `update_time` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='管理员表';
+
+#
+# Data for table "tp_admin"
+#
+
+
+#
+# Structure for table "tp_admin_role"
+#
+
+DROP TABLE IF EXISTS `tp_admin_role`;
+CREATE TABLE `tp_admin_role` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `admin_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '管理员ID',
+  `role_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '角色ID',
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `update_time` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='管理员角色关联表';
+
+#
+# Data for table "tp_admin_role"
+#
+
+
+#
+# Structure for table "tp_article"
+#
+
+DROP TABLE IF EXISTS `tp_article`;
+CREATE TABLE `tp_article` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL DEFAULT '' COMMENT '标题',
+  `keywords` varchar(255) DEFAULT NULL COMMENT '关键字',
+  `description` varchar(255) DEFAULT NULL COMMENT '描述',
+  `content` text NOT NULL COMMENT '内容',
+  `category_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '栏目ID',
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `update_time` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文章表';
+
+#
+# Data for table "tp_article"
+#
+
+
+#
+# Structure for table "tp_category"
+#
+
+DROP TABLE IF EXISTS `tp_category`;
+CREATE TABLE `tp_category` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '' COMMENT '名称',
+  `type` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '类型:1列表,2单页,3链接',
+  `keywords` varchar(255) DEFAULT NULL COMMENT '关键字',
+  `description` varchar(255) DEFAULT NULL COMMENT '描述',
+  `content` text COMMENT '内容',
+  `sort` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '排序',
+  `parent_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '上级ID',
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `update_time` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='栏目表';
+
+#
+# Data for table "tp_category"
+#
+
+
+#
+# Structure for table "tp_config"
+#
+
+DROP TABLE IF EXISTS `tp_config`;
+CREATE TABLE `tp_config` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL DEFAULT '' COMMENT '标题',
+  `name` varchar(50) NOT NULL DEFAULT '' COMMENT '变量名',
+  `type` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '类型:1单行文本,2多行文本,3单选按钮,4复选框,5下拉框',
+  `value` varchar(255) DEFAULT NULL COMMENT '配置值',
+  `values` varchar(255) DEFAULT NULL COMMENT '值',
+  `create_time` int(11) unsigned NOT NULL DEFAULT '0',
+  `update_time` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='配置表';
+
+#
+# Data for table "tp_config"
+#
+
+
+#
+# Structure for table "tp_permissions"
+#
+
+DROP TABLE IF EXISTS `tp_permissions`;
+CREATE TABLE `tp_permissions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL DEFAULT '' COMMENT '标题',
+  `icon` varchar(50) DEFAULT NULL COMMENT '图标',
+  `path` varchar(255) DEFAULT NULL COMMENT '路径',
+  `menu` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '菜单:0不是,1是',
+  `sort` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '排序',
+  `parent_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '上级ID',
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `update_time` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='权限表';
+
+#
+# Data for table "tp_permissions"
+#
+
+
+#
+# Structure for table "tp_role"
+#
+
+DROP TABLE IF EXISTS `tp_role`;
+CREATE TABLE `tp_role` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '' COMMENT '名称',
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '状态:0禁用,1正常',
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `update_time` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色表';
+
+#
+# Data for table "tp_role"
+#
+
+
+#
+# Structure for table "tp_role_permissions"
+#
+
+DROP TABLE IF EXISTS `tp_role_permissions`;
+CREATE TABLE `tp_role_permissions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '角色ID',
+  `permissions_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '权限ID',
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `update_time` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色权限关联表';
+
+#
+# Data for table "tp_role_permissions"
+#
+
